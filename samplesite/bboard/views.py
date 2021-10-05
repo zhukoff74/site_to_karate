b8c267bda6e475d638acc4ca1ddb2ff777f9747b
zhukoff74 <zhukoffwork@gmail.com>
2021-10-05 21:24:57 +0300

    page 53, the end part 1

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Bb
+
+
+def index(request):
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
