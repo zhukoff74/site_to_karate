3c922f9657870d8d6af2e05d5a09c20d1becb92d
zhukoff74 <zhukoffwork@gmail.com>
2021-10-06 20:34:14 +0300

    64 page

---
+++ samplesite/bboard/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+
+
+from .models import Bb
+
+
+class BbForm(ModelForm):
+    class Meta:
+        model = Bb
+        fields = ('title', 'content', 'price', 'rubric')
+
